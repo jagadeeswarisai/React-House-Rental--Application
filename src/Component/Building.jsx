67e1ed16a4c54c87fdae49045779bd526cadd33b
rajeammu <raje30pa@gmail.com>
2023-08-21 16:29:55 +0530

    Add new components

--- src/Component/Building.jsx
+++ src/Component/Building.jsx
@@ -8,7 +8,7 @@ function Building()
         <nav>
       <div class="header">
         <div class="Dash">
-        <Link id="dash"to="/dashboard/building/buildinlist">List Of Building Owner</Link>
+        <Link id="dash"to="/dashboard/building/buildinglist">List Of Building Owner</Link>
         </div>
         <div class="own">
         <Link id="own"to="/dashboard/building/buildingowners">+Add New Owners</Link>
